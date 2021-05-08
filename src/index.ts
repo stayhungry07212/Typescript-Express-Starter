da547b42eef14f8d455b17b06b53aa3f6cd56e46
stayhungry07212 <stay.hungry07212@gmail.com>
2021-05-09 00:42:27 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import * as http from 'http'
+import { DB_CONNECTION_STRING, PORT, USE_DB } from './var/config'
+import { Socket } from './socket/index'
+import app from './server'
+
+const server: http.Server = http.createServer(app)
+const socket = new Socket(server)
+
+server.listen(PORT)
+
+server.on('error', (e: Error) => {
+  console.log('Error starting server' + e)
+})
+
+server.on('listening', () => {
+  if (USE_DB) {
+    console.log(
+      `Server started on port ${PORT} on env ${process.env.NODE_ENV ||
+        'dev'} dbcon ${DB_CONNECTION_STRING}`,
+    )
+  } else {
+    console.log(
+      `Server started on port ${PORT} on env ${process.env.NODE_ENV ||
+        'dev'}`,
+    )
+  }
+})
+
+export default {
+  server,
+  socket,
+}
